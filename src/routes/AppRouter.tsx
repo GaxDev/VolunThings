463fb38aa35f7ecf8a6d9adb3c47405b40b1c91f
Kike Ruiz <kikeruuuiz@gmail.com>
2026-05-14 10:56:28 +0200

    feat: arreglando login e inicio de sesión

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Materials from "../pages/Materials";
@@ -7,6 +7,11 @@ import Navbar from "../components/Navbar";
 import Register from "../auth/Register";
 import Login from "../auth/Login";
 import { ToastContainer } from "react-toastify";
+
+const ProtectedRoute = () => {
+  const token = localStorage.getItem("token");
+  return token ? <Outlet /> : <Navigate to="/login" replace />;
+};
 
 const MainLayout = () => (
   <>
@@ -30,11 +35,13 @@ const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/materials" element={<Materials />} />
-          <Route path="/loans" element={<Loans />} />
-          <Route path="/about" element={<About />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/materials" element={<Materials />} />
+            <Route path="/loans" element={<Loans />} />
+            <Route path="/about" element={<About />} />
+          </Route>
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
